c6ee4472c8c54f59a96e9888be89edb0f4285717
vinibelz <166419024+vinibelz@users.noreply.github.com>
2024-11-04 11:22:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'one piece',
+    'qual o nome da filha do Shanks?',
+    'Uta'
+)
+criaCartao(
+    'one piece',
+    'quem faz parte do trio monstro?',
+    'Luffy, Zoro, Sanji'
+)
+criaCartao(
+    'one piece',
+    'qual o nome do imediato dos pirtas do coração?',
+    'Bepo'
+)
+criaCartao(
+    'one piece',
+    'qual o nome da fruta do Barba Branca?',
+    'Gura Gura no mi, fruta do terremoto'
+)
+criaCartao(
+    'one piece',
+    'quantos membros há na tripulação dos mugiwaras?',
+    '10 tripulantes'
+)
+criaCartao(
+    'one piece',
+    'quem é ASL?',
+    'Ace, Sabo e Luffy'
+)
